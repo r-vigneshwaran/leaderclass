a20e510167d8e83f5295ad217921f6e7d0172efc
vignesh waran <vikivarun7@gmail.com>
2021-03-06 08:26:29 +0530

    lc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import "./styles.css";
+import Header from './Components/Header/Header'
+import './App.css';
+import {useState} from 'react'
+import Testimonials from './Components/Testimonials/Testimonials';
+import Inspiration from './Components/Inspiration/Inspiration';
+import Biography from './Components/Biography/Biography';
+import Footer from './Components/Footer/Footer';
+import SideDrawer from './Components/SideDrawer/SideDrawer';
 
-export default function App() {
+function App() {
+  const [isOpen,setIsOpen]=useState(false)
+  const HandleToggleClick=()=>{
+    setIsOpen(!isOpen)
+  }
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div style={{height:'100%'}}>
+    <Header HandleToggleClick={HandleToggleClick}/>
+    <SideDrawer show={isOpen} />
+    <Testimonials/>
+    <Inspiration/>
+    <Biography/>
+    <Footer/>
     </div>
   );
 }
+
+export default App;
